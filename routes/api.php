82997f8a1c4b09f131a5d6a1683918ba665829d6
Rakishly <630390925@qq.com>
2020-08-03 15:17:57 +0800

    添加api接口

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,6 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+Route::middleware('throttle:60,1')->prefix('v1')->group(function () {
+    Route::get('/articles', 'API\PostController@index');
+});
